c162b87453868f383f3a638bda990f9a547d9015
ernurQ <garifullin.ernur@gmail.com>
2024-08-31 17:18:17 +0500

    feat(app): setup swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory, Reflector } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
@@ -8,6 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
   const port = configService.get<number>('app.port')!
+
+  const config = new DocumentBuilder()
+    .setTitle('Planner backend API')
+    .setVersion('1.0')
+    .addTag('Planner backend API')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
 
   app.useGlobalPipes(
     new ValidationPipe({
