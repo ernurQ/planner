b85bac4d36188623c02517e1d3643fb57306bad6
ernurQ <garifullin.ernur@gmail.com>
2024-08-31 17:12:35 +0500

    feat(app): add Interceptors and pipes to serialize and validate DTOs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { NestFactory } from '@nestjs/core'
+import { NestFactory, Reflector } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
@@ -7,6 +8,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
   const port = configService.get<number>('app.port')!
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
+      excludeExtraneousValues: true,
+    }),
+  )
 
   await app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
