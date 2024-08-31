ea3da839d0b346838cc5357a5238934795c8e2fe
ernurQ <garifullin.ernur@gmail.com>
2024-08-31 16:32:59 +0500

    fix(config): fix field types autoLoadEntities, synchronize to Boolean

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -16,6 +16,6 @@ export const DatabaseConfig = registerAs('database', () => ({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   name: process.env.DATABASE_NAME,
-  autoLoadEntities: process.env.DATABASE_AUTO_LOAD_ENTITIES,
-  synchronize: process.env.DATABASE_SYNCHRONIZE,
+  autoLoadEntities: process.env.DATABASE_AUTO_LOAD_ENTITIES === 'true',
+  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
 }))
