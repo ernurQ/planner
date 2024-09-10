a4596c0df95aa7ff30159d61ce8b5b817582afa4
ernurQ <garifullin.ernur@gmail.com>
2024-09-10 19:22:01 +0500

    fix(notes): add api tag 'notes'

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 
 import { CreateNoteDto } from '@Notes/dto'
 import { NotesService } from '@Notes/notes.service'
 import { JwtAuth, JwtPayload } from '@Shared/decorators'
 import { JwtTokenPayload } from '@Shared/types'
 
+@ApiTags('Notes')
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
