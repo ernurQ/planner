05e08511b1b123796010923224b5e9367adcd752
ernurQ <garifullin.ernur@gmail.com>
2024-09-13 17:54:22 +0500

    refactor(main): refactor main.ts for setup and configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,8 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory, Reflector } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
@@ -7,9 +11,19 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  setupSwagger(app)
+  setupDTOSerialization(app)
+
   const configService = app.get(ConfigService)
   const port = configService.get<number>('app.port')!
 
+  await app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+  })
+}
+
+function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('Planner backend API')
     .setVersion('1.0')
@@ -17,7 +31,9 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, document)
+}
 
+function setupDTOSerialization(app: INestApplication) {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -32,9 +48,6 @@ async function bootstrap() {
       excludeExtraneousValues: true,
     }),
   )
-
-  await app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-  })
 }
+
 bootstrap()
