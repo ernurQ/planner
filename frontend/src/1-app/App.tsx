86a275258143a98c12ce2a4f580cdb85589e6d34
ernurQ <garifullin.ernur@gmail.com>
2024-09-14 23:37:07 +0500

    refactor(frontend): update App component and add router configuration

--- frontend/src/1-app/App.tsx
+++ frontend/src/1-app/App.tsx
@@ -1,7 +1,15 @@
-import '@App/styles/index.scss'
+import { RouterProvider } from 'react-router-dom'
+
+import router from '@App/router'
+
+import './styles/index.scss'
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App
