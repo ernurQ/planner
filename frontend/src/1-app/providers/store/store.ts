cad82f866fdb57c92abe5907e2458ca1a52e61d0
ernurQ <garifullin.ernur@gmail.com>
2024-09-17 23:18:14 +0500

    feat(frontend): integrate planner API into store configuration

--- frontend/src/1-app/providers/store/store.ts
+++ frontend/src/1-app/providers/store/store.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { plannerApi } from '@Shared/api'
 
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    [plannerApi.reducerPath]: plannerApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(plannerApi.middleware),
 })
