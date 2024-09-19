a235aadaee7aa2b775d79e4d9b1f7e1b9b3242d1
ernurQ <garifullin.ernur@gmail.com>
2024-09-19 22:33:27 +0500

    feat(frontend): add api endpoints

--- frontend/src/6-shared/config/api.ts
+++ frontend/src/6-shared/config/api.ts
@@ -1,3 +1,19 @@
 export const apiBaseUrl = 'http://localhost:3000/api'
 
-export const apiEndpoints = {}
+export const apiEndpoints = {
+  register: () => '/auth/register',
+  login: () => '/auth/login',
+  getMyPlans: () => '/plans',
+  getUserPlans: (username: string) => `/plans/${username}`,
+  getPlan: (ownerName: string, planTitle: string) =>
+    `/plans/${ownerName}/${planTitle}`,
+  createNote: () => '/notes',
+  createTask: () => '/tasks',
+  updatePlan: (planTitle: string) => `/plans/${planTitle}`,
+  updatePlanIsPrivate: (planTitle: string) => `/plans/${planTitle}/is-private`,
+  updatePlanIsTemplate: (planTitle: string) =>
+    `/plans/${planTitle}/is-template`,
+  deletePlan: (planTitle: string) => `/plans/${planTitle}`,
+  createPlan: () => '/plans',
+  createPlanWithTemplate: () => '/templates',
+}
